575b78edd43461fd6c3bb214b9e8c9e06f0c29c3
thelostword <thelostword@163.com>
2023-09-20 09:28:36 +0000

    Deploying to gh-pages from @ thelostword/cub-admin-template@267fda6641b0527e14d2f2c1cb6462c54d419511 🚀

---
+++ assets/not-found-50526b57-30903705.js
@@ -0,0 +1,1 @@
+import{d as t,u as n,h as o,I as u}from"./index-07bfb478.js";const s=t({name:"CubPageNotFound",setup(){const e=n();return()=>o("div",{},[o("span",{onclick:()=>{e.back(),u(e.currentRoute.value)}},"返回上一页")])}});export{s as default};
